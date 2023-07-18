b566746c1fefa566103643934f4f4908391dc4e7
luciaferrari2 <139228762+luciaferrari2@users.noreply.github.com>
2023-07-18 16:34:05 +0200

    Inserimento ID_Utente nel registro
    Inserimento dell'ID_Utente nel registro per permettere una serie di incolonnamenti successivi nel DataFrame.

--- registro.py
+++ registro.py
@@ -19,7 +19,7 @@ def generaPassword(lunghezza):
 
 class Utente:
     def __init__(self, nome, cognome, data_nascita, indirizzo, num_telefono, saldo = 0, \
-                 registro = [[datetime.datetime.now(), "creazione conto", 0, 0]]):
+                 registro = [[0, datetime.datetime.now(), "creazione conto", 0, 0]]):
         """Classe Utente:
         Args:
             nome (str) : nome utente
@@ -30,8 +30,8 @@ class Utente:
             saldo (int) : saldo attuale, 
                             default =0
             registro (list) : registro operazioni, 
-                            lista contenente liste del tipo [data operazione (datetime), nome operazione(str), operazione (int), saldo (int)], 
-                            default = [[data creazione conto, "creazione conto", 0, 0]]
+                            lista contenente liste del tipo [Id_utente (int), data operazione (datetime), nome operazione(str), operazione (int), saldo (int)], 
+                            default = [[0, data creazione conto, "creazione conto", 0, 0]]
             """
         self.nome = nome
         self.cognome = cognome
@@ -47,6 +47,7 @@ class Utente:
             numero_utenti (int) : numero utenti già registrati 
         """
         self.utente = numero_utenti + 1
+        self.registro[0][0]= self.utente
         self.__password = generaPassword(8)
         print(f"Registrato!\n Utente: {self.utente}\n Password: {self.__password}")
     
@@ -59,7 +60,7 @@ class Utente:
             deposito (int) : somma di denaro che si vuole depositare
         """
         self.saldo += deposito
-        self.registro.append([datetime.datetime.now(), "deposito", deposito, self.saldo])
+        self.registro.append([self.utente, datetime.datetime.now(), "deposito", deposito, self.saldo])
         print(f"Operazione riuscita! Saldo attuale: {self.saldo}€\n")
 
     def preleva(self, prelievo):
@@ -71,14 +72,14 @@ class Utente:
             prelievo (int) : somma di denaro che si vuole prelevare
         """
         self.saldo -= prelievo
-        self.registro.append([datetime.datetime.now(), "prelievo", - prelievo, self.saldo])
+        self.registro.append([self.utente, datetime.datetime.now(), "prelievo", - prelievo, self.saldo])
         print(f"Operazione riuscita! Saldo attuale: {self.saldo}€\n")
 
     def stampaReport(self):
         print("OPERAZIONI EFFETTUATE:\n")
         for riga in self.registro:
-            data_str = riga[0].strftime("%d/%m/%Y")
-            print(f"Data: {data_str}, Operazione: {riga[1]} di {riga[2]}€, Saldo: {riga[3]} \n")
+            data_str = riga[1].strftime("%d/%m/%Y")
+            print(f"Data: {data_str}, Operazione: {riga[2]} di {riga[3]}€, Saldo: {riga[4]}€ \n")
 
 data_nascita1 = datetime.date(1998, 12, 2)
 
@@ -86,7 +87,10 @@ utente1 = Utente("Lucia", "Ferrari", data_nascita1, "via blabla", 3323458192 )
 utente1.creaCredenziali(0)
 utente1.deposita(1000)
 utente1.preleva(50)
+utente1.deposita(20)
+utente1.preleva(200)
 utente1.stampaReport()
+print(utente1.registro)
 #print(utente1.registro)
 
 
