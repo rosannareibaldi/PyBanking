2c8c8a4319e00532367c6f3f8d2a4baf9fb4f4c0
luciaferrari2 <139228762+luciaferrari2@users.noreply.github.com>
2023-07-18 14:22:59 +0200

    classe utente

---
+++ registro.py
@@ -0,0 +1,47 @@
+import getpass
+import random
+import string
+
+def genera_password(lunghezza):
+    """ Genera una password della lunghezza richiesta, prendendo randomicamente caratteri tra lettere, numeri e punteggiatura.
+
+    Args:
+        lunghezza: lunghezza della password
+
+    Returns:
+        password: password randomica
+    """
+    caratteri = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(caratteri) for _ in range(lunghezza))
+    return password
+
+class Utente:
+    def __init__(self, nome, cognome, data_nascita, indirizzo, num_telefono):
+        self.nome = nome
+        self.cognome = cognome
+        self.data_nascita = data_nascita
+        self.indirizzo = indirizzo
+        self.num_telefono = num_telefono
+        self.saldo = 0
+    
+    def creaCredenziali(self,numero_utenti):
+        """ Crea le credenziali utente e password.
+        Args:
+            numero_utenti: numero utenti già registrati
+            
+        Aggiorna l'oggetto della classe Utente inserendo utente e password
+        """
+        self.utente = numero_utenti + 1
+        self.__password = genera_password(8)
+    
+    def deposita(self,deposito):
+        self.saldo += deposito
+
+    def prelieva(self,prelievo):
+        self.saldo -= prelievo
+
+
+ #   def trasferisci():
+
+ #   def stampaReport():
+
